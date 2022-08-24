8d6576e1336c1a57ca4a75f5336fab1fb6b0328c
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-24 22:57:54 +0530

    Added parameter as path variable.

--- FirstStringApp/src/main/java/com/bridgelabz/firstspringapp/controller/HelloRestController.java
+++ FirstStringApp/src/main/java/com/bridgelabz/firstspringapp/controller/HelloRestController.java
@@ -1,14 +1,22 @@
 package com.bridgelabz.firstspringapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
 
+    //http://localhost:8080/hello/home
     @RequestMapping(value = { "/home"})
     public String sayHello(){
         return "Hello From Bridgelabz!!!";
+    }
+    // http://localhost:8080/hello/query?name=Mark
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name")String name){
+        return "Hello " + name + " ! ";
     }
 }
